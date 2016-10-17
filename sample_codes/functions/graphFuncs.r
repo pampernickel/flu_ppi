b86e9b751e2e69a568ea57a694bcbb190d0e4bce
Maria Pamela Dobay <pamdobay@pams-macbook-air.home>
2016-10-18 01:10:26 +0200

    Added function for constructing graph from a list of proteins and a list of its network neighborhood; the graph is created from the PPI from which the network neighborhood was derived so as to retain any vertex or edge annotations in the source graph

---
+++ sample_codes/functions/graphFuncs.r
@@ -0,0 +1,98 @@
+##========================================================
+##
+##  Credits:
+##  Code written by MPDobay for flu interaction network analysis
+##  SystemsX project: BCF/SIB
+##
+##========================================================
+
+source('./sample_codes/functions/routineFuncs.r')
+
+
+calcSimilarity <- function(graph.1, graph.2){
+  # http://lists.nongnu.org/archive/html/igraph-help/2008-04/msg00017.html
+  # --- pairwise comparison of graphs in terms of overlapping edges,
+  # --- implicitly, edges
+  # sum(get.adjacency(sp.g$subgraph) != get.adjacency(mst.g.0$subgraph))
+  # works only with graphs of the same size
+  # graph.intersection(sp.g$subgraph, mst.g.0$subgraph)
+  # doesn't work
+  
+  length(E(graph.2))+length(E(graph.1)) -> edge.tot
+  # --- for each graph, check overlapping edge sequences
+  get.edgelist(graph.1) -> graph.1
+  get.edgelist(graph.2) -> graph.2
+  
+  apply(graph.1, 1, function(x) checkEdge(x[1], x[2], graph.2)) -> in.2
+  apply(graph.2, 1, function(x) checkEdge(x[1], x[2], graph.1)) -> in.1
+  
+  length(c(which(in.1 %in% TRUE), which(in.2 %in% TRUE)))/edge.tot -> similarity.score
+  return(similarity.score)
+}
+
+calcVertexSimilarity <- function(graph.1, graph.2){
+  # --- pairwise comparison of graphs in terms of overlapping
+  # --- vertices, regardless of edge included
+  length(V(graph.1)$name)+length(V(graph.2)$name) -> v.tot
+  length(which(V(graph.1)$name %in% V(graph.2)$name))+
+    length(which(V(graph.2)$name %in% V(graph.1)$name)) -> subtot
+  subtot/v.tot -> similarity
+  return(similarity)
+}
+
+
+getNeighbors <- function(graph, queries, ord=1){
+  # wrapper for igraph.neighborhood function
+  unique(unlist(c(which(V(graph)$name %in% queries), 
+                  ego(graph, which(V(graph)$name %in% queries), order=ord)))) -> ind
+  return(V(graph)$name[ind])
+}
+
+getNodeAttribute <- function(graph, attribute, node){
+  # get attribute for a given node in a graph
+  a <- get.vertex.attribute(graph, attribute, index=which(V(graph)$name %in% node))
+  if (length(a) > 0 && (attribute %in% "gos" || attribute %in% "gos.cc")){
+    a <- strsplit(a, " ")[[1]][-which(strsplit(a, " ")[[1]] %in% "")]
+  }
+  return(a)
+}
+
+subgraphFromDataFrame <- function(graph, df){
+  # given a df, find corresponding edges in graph
+  # and create a subgraph from these edges
+  apply(df, 1, function(x)
+    get.edge.ids(graph, c(as.character(x[1]), as.character(x[2])))) -> ind
+  unlist(ind) -> ind
+  if (length(which(ind %in% 0))>0){
+    ind[-which(ind %in% 0)] -> ind
+  }
+  graph.sub <- subgraph.edges(graph, as.numeric(ind), delete.vertices = TRUE)
+  return(graph.sub)
+}
+
+constructGraph <- function(nn, ppis, p){
+  # creates a graph from a list nn; each slot of nn contains 
+  # a list of neighbors of some proteins p
+  # length of nn[[i]] == length(p)
+  # then, for each constructed subgraph, get
+  # induce a subgraph from the original (so that attributes are
+  # maintained)
+  lapply(nn, function(x){
+    gdf <- matrix(0, nrow=0, ncol=2)
+    colnames(gdf) <- c("source", "target")
+    for (i in 1:length(p)){
+      x[[i]][which(x[[i]] %ni% p[i])] -> fin
+      cbind(rep(p[i], length(fin)), fin) -> t
+      colnames(t) <- colnames(gdf)
+      rbind(gdf, t) -> gdf
+    }
+    return(gdf)
+  }) -> sg.df
+  
+  lapply(1:length(sg.df), function(x){
+    apply(sg.df[[x]], 1, function(y) get.edge.ids(ppis[[x]], y, directed = F)) -> eids
+    subgraph.edges(ppis[[x]], eids, delete.vertices = T) -> sg
+  }) -> sg
+  
+  return(sg)
+}
