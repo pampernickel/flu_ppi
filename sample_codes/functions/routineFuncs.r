88a38e49ded92a5f5e77ec5a7c217d08953c4293
Pam Dobay <maria.pamela.david@gmail.com>
2016-11-14 00:15:29 +0100

    Syntax correction

--- sample_codes/functions/routineFuncs.r
+++ sample_codes/functions/routineFuncs.r
@@ -96,7 +96,7 @@ source_https <- function(url, ...) {
   })
 }
 
-loadDependencies() <- function(){
+loadDependencies <- function(){
   print("Loading scripts from pampernickel/flu_ppi...")
   source_https('https://raw.githubusercontent.com/pampernickel/flu_ppi/master/sample_codes/functions/graphFuncs.r')
   source_https('https://github.com/pampernickel/flu_ppi/blob/master/sample_codes/functions/GOFuncs.r')
