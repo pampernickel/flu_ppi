922e67475781c0bd1d952865860032122cab74bc
Pam Dobay <maria.pamela.david@gmail.com>
2016-11-14 12:40:00 +0100

    Converted 'hgnc' from global to passed variable

--- sample_codes/functions/GOFuncs.r
+++ sample_codes/functions/GOFuncs.r
@@ -11,20 +11,20 @@ library(GO.db)
 library(org.Hs.eg.db)
 
 # --- retrieve gos
-getgos <- function(xx){
+getgos <- function(xx, hgnc){
   # --- append GO terms to graph
   sql <- "SELECT DISTINCT go_id FROM go_bp"
   dbGetQuery(org.Hs.eg_dbconn(),sql) -> goss
   which(names(xx) %in% goss[,1]) -> ind
   xx[ind] -> xx
-  entrez2symbol(xx) -> xx.s
+  entrez2symbol(xx, hgnc) -> xx.s
   unique(unlist(xx.s)) -> all.ids
   list(xx, xx.s) -> gos
   names(gos) <- c("entrez", "symbol")
   return(gos)
 }
 
-getgos_cc <- function(xx){
+getgos_cc <- function(xx, hgnc){
   suppressPackageStartupMessages({require('org.Hs.eg.db', quietly=TRUE)})
   suppressPackageStartupMessages({require(igraph, quietly=TRUE)})
   # --- append GO terms to graph
@@ -32,7 +32,7 @@ getgos_cc <- function(xx){
   dbGetQuery(org.Hs.eg_dbconn(),sql) -> GO.ccs
   which(names(xx) %in% GO.ccs[,1]) -> ind
   xx[ind] -> xx
-  entrez2symbol(xx) -> xx.s
+  entrez2symbol(xx, hgnc) -> xx.s
   unique(unlist(xx.s)) -> all.ids
   list(xx, xx.s) -> gos
   names(gos) <- c("entrez", "symbol")
